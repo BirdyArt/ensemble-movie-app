b3717b1ab11401d5b96e0b3112f1245ff1f58420
BirdyArt <asobolev91@gmail.com>
2022-04-22 13:08:17 -0700

    feat: init setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import HomePage from './pages/HomePage';
+
+export default function App() {
+  return (
+		<Routes>
+			<Route exact path="/" element={<HomePage/>} />
+		</Routes>
+  )
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div className="flex justify-center">
+			<h1 className="font-bold text-2xl text-blue-200 mt-4">
+				Ensemble Movie App boilerplate!
+			</h1>
+		</div>
+  )
+}
